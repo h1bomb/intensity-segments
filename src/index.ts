0a2d427b84665713e52954a1be6dcb253f5a71d2
Qiqi Zhou <qiqi.zhou@gmail.com>
2024-11-30 11:45:59 +0800

    refactor: clean up index.ts to export library interface
    Remove example code and properly export IntensitySegments class for library use

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,4 @@
-import { IntensitySegments } from './IntensitySegments';
+export { IntensitySegments } from './IntensitySegments';
 
-let segments = new IntensitySegments();
-console.log(segments.toString()); // Should be "[]"
-segments.add(10, 30, 1);
-console.log(segments.toString()); // Should be: "[[10,1],[30,0]]"
-segments.add(20, 40, 1);
-console.log(segments.toString()); // Should be: "[[10,1],[20,2],[30,1],[40,0]]"
-segments.add(10, 40, -2);
-console.log(segments.toString()); // Should be: "[[10,-1],[20,0],[30,-1],[40,0]]"
 
-// Another example sequence:
-segments = new IntensitySegments();
-console.log(segments.toString()); // Should be "[]"
-segments.add(10, 30, 1);
-console.log(segments.toString()); // Should be "[[10,1],[30,0]]"
-segments.add(20, 40, 1);
-console.log(segments.toString()); // Should be "[[10,1],[20,2],[30,1],[40,0]]"
-segments.add(10, 40, -1);
-console.log(segments.toString()); // Should be "[[20,1],[30,0]]"
-segments.add(10, 40, -1);
-console.log(segments.toString()); // Should be "[[10,-1],[20,0],[30,-1],[40,0]]"
 
